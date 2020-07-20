267e95dba84166a52c26123e21603ba0634eafbe
石嶺利樹 <toshiki@MacBook-Pro.local>
2020-07-21 01:56:19 +0900

    "ビンゴカード完成"

--- JavaScript/MyBingo/js/main.js
+++ JavaScript/MyBingo/js/main.js
@@ -7,21 +7,35 @@
             source[i] = i + 1 +15 * col;
         }
 
-        const columns = [];
+        const column = [];
         for (let i = 0; i < 5; i++) {
-            columns[i] = source.splice((Math.floor(Math.random() * source.length)), 1)[0];
+            column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
         }
 
+        return column;
+    }
+
+    function createColumns() {
+        const columns = [];
+        for(let i = 0; i < 5; i++){
+            columns[i] = createColumn(i)
+        }
+        columns[2][2] = 'FREE';
         return columns;
     }
 
-    const columns = [];
-    columns[0] = createColumn(0);
-    columns[1] = createColumn(1);
-    columns[2] = createColumn(2);
-    columns[3] = createColumn(3);
-    columns[4] = createColumn(4);
-    columns[2][2] = 'FREE';
+    function renderBingo(columns) {
+        for (let row = 0; row < 5; row++) {
+            const tr = document.createElement('tr');
+            for (let col = 0; col < 5; col ++) {
+                const td = document.createElement('td');
+                td.textContent = columns[col][row];
+                tr.appendChild(td);
+            }
+            document.querySelector('tbody').appendChild(tr);
+        }
+    }
 
-    console.table(columns);
-}
+    const columns = createColumns();
+    renderBingo(columns);
+  }
